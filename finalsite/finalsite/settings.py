cbee48e4d5b138dc992b311788c5b1cf85dd767a
DarsVelasco <darwin.velasco1210@gmail.com>
2024-01-02 14:25:45 +0800

    add seller cards

--- finalsite/finalsite/settings.py
+++ finalsite/finalsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-78gab^lz1qc%f$u^kp*&l+lx&qj#tyxoadn!w6j2pil4*0pa+0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
